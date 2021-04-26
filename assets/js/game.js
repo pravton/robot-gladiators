b1759128430410f41a78fdab4a2761f775d50ec1
PravTon <clintonsf@outlook.com>
2021-04-26 19:07:43 -0400

    log update

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is a alrt! Javascript is running!")
+
+//Promting the Name
+var playerName = window.prompt("What is your robot's name?");
+
+//Note the lack of quotation marks around the playerName
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a funtion named "fight"
+function fight() {
+    window.alert("The fight has begun")
+}
+
+//fight();
